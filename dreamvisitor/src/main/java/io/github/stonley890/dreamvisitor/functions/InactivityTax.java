8b883de18a2e2d593d35c95e503c2a9e7f7a0363
Stonley890 <stonley@macver.org>
2025-04-09 14:25:39 -0700

    Add frameworks
    Add necessary config values, create InactivityTax.java, and extract getLastLogin and getLastLogout from DCmdSeen.java to PlayerUtility.java for convenience.

---
+++ dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/functions/InactivityTax.java
@@ -0,0 +1,12 @@
+package io.github.stonley890.dreamvisitor.functions;
+
+public class InactivityTax {
+
+    /**
+     * This function should run once per day to tax inactive players.
+     */
+    public static void tax() {
+
+    }
+
+}

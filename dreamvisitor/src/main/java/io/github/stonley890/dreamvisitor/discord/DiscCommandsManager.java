7777e0e97de05848868bf6951b3ba582d29498bb
Stonley <stonley@macver.org>
2025-04-11 14:23:12 -0700

    Remove year tracker

--- dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/discord/DiscCommandsManager.java
+++ dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/discord/DiscCommandsManager.java
@@ -57,7 +57,6 @@ public class DiscCommandsManager extends ListenerAdapter {
         addList.add(new DCmdWork());
         addList.add(new DCmdBaltop());
         addList.add(new DCmdSeen());
-        addList.add(new DCmdYear());
 
         Dreamvisitor.debug("Ready to add to guild.");
 

e74f3edd31fffc30a24d5f7d07a262c919188044
Bog <bog@macver.org>
2025-09-02 10:32:25 -0700

    Cancel delivery when entering Nether

--- dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/listeners/ListenPlayerChangedWorld.java
+++ dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/listeners/ListenPlayerChangedWorld.java
@@ -1,6 +1,7 @@
 package io.github.stonley890.dreamvisitor.listeners;
 
 import io.github.stonley890.dreamvisitor.functions.Flight;
+import io.github.stonley890.dreamvisitor.functions.Mail;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
@@ -10,6 +11,10 @@ public class ListenPlayerChangedWorld implements Listener {
 
     @EventHandler
     public void onPlayerChangedWorld(@NotNull PlayerChangedWorldEvent event) {
+        if (Mail.isPLayerDeliverer(event.getPlayer())) {
+            // Cancel parcel if going to nether
+            if (event.getPlayer().getWorld().isUltraWarm()) Mail.cancel(event.getPlayer());
+        }
         Flight.setupFlight(event.getPlayer());
     }
 

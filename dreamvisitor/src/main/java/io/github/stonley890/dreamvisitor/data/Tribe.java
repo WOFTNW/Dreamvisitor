be7d671cbb01cc7f30eb4a31d21b326897244b85
fwfy <fwfy@fwfy.club>
2026-09-21 20:35:20 -0400

    feat: add discord emoji icons for each tribe

--- dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/data/Tribe.java
+++ dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/data/Tribe.java
@@ -22,6 +22,8 @@ public enum Tribe {
     private String name = "Undefined.";
     @NotNull
     private ChatColor color = ChatColor.WHITE;
+    @NotNull
+    private String icon = "\uD83D\uDC09"; // Dragon emoji by default
 
     static {
         HIVE.name = "Hive";
@@ -47,6 +49,18 @@ public enum Tribe {
         SILK.color = ChatColor.LIGHT_PURPLE;
         SKY.color = ChatColor.DARK_RED;
         HUMAN.color = ChatColor.GRAY;
+
+        HIVE.icon = "<:HiveIcon:1059558826929569884>";
+        ICE.icon = "<:IceIcon:996925736508203058>";
+        LEAF.icon = "<:LeafIcon:1059555593096011796>";
+        MUD.icon = "<:MudIcon:996925738026537101>";
+        NIGHT.icon = "<:NightIcon:996925740476018758>";
+        RAIN.icon = "<:RainIcon:996925741562331136>";
+        SAND.icon = "<:SandIcon:996925743055507517>";
+        SEA.icon = "<:SeaIcon:996925744980688896>";
+        SILK.icon = "<:SilkIcon:1059548640810639361>";
+        SKY.icon = "<:SkyIcon:996925746436128809>";
+        HUMAN.icon = "\uD83E\uDDCD";
     }
 
     /**
@@ -78,4 +92,11 @@ public enum Tribe {
         return color;
     }
 
+    /**
+     * Get the icon of this tribe.
+     *
+     * @return The Discord emoji for this tribe.
+     */
+    @NotNull
+    public String getIcon() { return icon; }
 }
